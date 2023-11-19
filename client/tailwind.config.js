d6f94da9bc33f21256b2581d5dc57dfc38b06253
Bruce Hsu <brucehsu1126@gmail.com>
2023-11-18 17:38:58 -0800

    Add tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: 'jit',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        fingerPaint: ['Finger Paint', 'Play', 'Sans Serif'],
+        play: ['Play', 'Fingure Paint', 'Sans Serif'],
+      },
+      colors: {
+        primary: '#E51636',
+        secondary: '#F24F4F',
+        gray: '#D9D9D9',
+      },
+      boxShadow: {
+        custom:
+          'rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px',
+      },
+    },
+  },
+  plugins: [],
+};
