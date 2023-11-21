09d56539a2da1e1968a793c461bcf54a4d11b86a
Bruce Hsu <brucehsu1126@gmail.com>
2023-11-21 01:00:04 -0800

    Minor styling changes to header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 const Header: FC<HeaderProps> = ({ name }) => {
   return (
     <header className="grid-in-main-header border-b-4 bg-secondary p-5">
-      <h1 className="text-outline font-fingerPaint text-4xl text-white md:text-6xl xl:text-7xl">
+      <h1 className="text-outline text-center font-fingerPaint text-4xl text-white md:text-left md:text-4xl xl:text-6xl">
         {name}
       </h1>
     </header>
