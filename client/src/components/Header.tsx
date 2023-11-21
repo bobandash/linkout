efa2018659d881ac764776a119faf442ac06150a
Bruce Hsu <brucehsu1126@gmail.com>
2023-11-21 13:59:57 -0800

    Change header styling for desktop

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
 
 const Header: FC<HeaderProps> = ({ name }) => {
   return (
-    <header className="grid-in-main-header border-b-4 bg-secondary p-5">
+    <header className="grid-in-main-header border-b-4 bg-secondary p-5 md:border-b-0 md:bg-primary">
       <h1 className="text-outline text-center font-fingerPaint text-4xl text-white md:text-left md:text-4xl xl:text-6xl">
         {name}
       </h1>
