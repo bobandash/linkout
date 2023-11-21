a04f30b6c94dadbe7972ffa9ce1e656c4dfc1ac5
Bruce Hsu <brucehsu1126@gmail.com>
2023-11-21 15:29:50 -0800

    Change routes to /user to denote individual user

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,6 +5,11 @@ const userController = require('../controller/userController');
 
 router.post('/create', userController.create_user);
 router.post('/login', userController.log_in);
-router.get('/sign-in-status', userController.sign_in_status);
+
+/* for individual user operations */
+router.get('/user/sign-in-status', userController.sign_in_status);
+router.get('/user/username', userController.get_username);
+router.get('/user/email', userController.get_email);
+router.get('/user/profile', userController.get_profile);
 
 module.exports = router;
