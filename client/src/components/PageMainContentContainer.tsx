cbd8c2255087ebb8a9e1e7d66cfb3e838c0bff1f
Bruce Hsu <brucehsu1126@gmail.com>
2023-11-23 20:05:21 -0800

    Create community page w/out handling picture

--- client/src/components/PageMainContentContainer.tsx
+++ client/src/components/PageMainContentContainer.tsx
@@ -1,5 +1,3 @@
-import OnlineSidebar from './OnlineSidebar';
-import Sidebar from './Sidebar';
 import { FC } from 'react';
 
 interface PageTemplateProps {
@@ -8,7 +6,7 @@ interface PageTemplateProps {
 
 const PageMainContentContainer: FC<PageTemplateProps> = ({ children }) => {
   return (
-    <div className="bg-color_3 flex flex-grow flex-col p-5 md:p-7 lg:p-10 lg:pt-0">
+    <div className="bg-color_3 flex flex-grow flex-col p-5 md:p-7 lg:p-10 lg:pt-0 2xl:p-16 2xl:pt-0">
       {children}
     </div>
   );
--- client/src/pages/CreateCommunity/CreateCommunityForm.tsx
+++ client/src/pages/CreateCommunity/CreateCommunityForm.tsx
@@ -1,18 +1,61 @@
-import { useState } from 'react';
 import FormInput from '../../components/Form/FormInput';
 import FormTextArea from '../../components/Form/FormTextArea';
 import FormIconInput from '../../components/Form/FormIconInput';
+import PageMainContentContainer from '../../components/PageMainContentContainer';
+import useCommunity from './__hooks__/useCommunity';
+import FormError from '../../components/Form/FormError';
 
 const CreateCommunityForm = () => {
-  const [community, setCommunity] = useState({});
+  const {
+    community,
+    handleInputChange,
+    handleTextAreaChange,
+    handleSubmit,
+    errors,
+  } = useCommunity();
+
   return (
-    <div className="bg-color_3 flex-grow">
-      <form encType="multipart/form-data" className="p-5" noValidate>
-        <FormIconInput />
-        <FormInput name={'Community Name'} autoFocus={true} />
-        <FormTextArea name={'Description'} />
-      </form>
-    </div>
+    <PageMainContentContainer>
+      <div className="bg-color_3 flex-grow">
+        <form
+          encType="multipart/form-data"
+          noValidate
+          autoComplete="off"
+          onSubmit={async (e) => {
+            handleSubmit(e);
+          }}
+        >
+          <FormIconInput centered={true} />
+          {community.profilePic === '' && (
+            <button className="mx-auto block text-white">
+              Generate With Ai
+            </button>
+          )}
+          <FormInput
+            name={'Name'}
+            autoFocus={true}
+            handleInputChange={handleInputChange}
+            value={community.name}
+            required={true}
+          />
+          {errors.name && <FormError message={errors.name.msg} />}
+          <FormTextArea
+            name={'Description'}
+            handleChange={handleTextAreaChange}
+            value={community.description}
+            rows={4}
+            required={true}
+          />
+          {errors.description && <FormError message={errors.description.msg} />}
+          <button
+            type="submit"
+            className="ml-auto mt-5 block bg-primary px-5 py-3 text-white"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </PageMainContentContainer>
   );
 };
 
