b540ac644c724e4c5365578a48f066cfc5e4e482
Bruce Hsu <brucehsu1126@gmail.com>
2023-11-23 20:03:02 -0800

    Add loading screen

--- client/src/pages/Loading/index.tsx
+++ client/src/pages/Loading/index.tsx
@@ -1,5 +1,13 @@
+import { Audio } from 'react-loader-spinner';
+
 const Loading = () => {
-  return <div className="text-center">Loading</div>;
+  return (
+    <div className="bg-color_3 min-h-screen w-screen">
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <Audio height="80" width="80" color="white" ariaLabel="loading" />
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
