d85ee36f946ec61b1d9a3be9f6301772a295c0f3
Bruce Hsu <brucehsu1126@gmail.com>
2023-11-24 16:20:24 -0800

    Add handle form submission, error, and success

--- client/src/pages/OwnProfile/__hooks__/useProfile.ts
+++ client/src/pages/OwnProfile/__hooks__/useProfile.ts
@@ -1,11 +1,27 @@
 import { useEffect, useState, useCallback } from 'react';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+
+interface profileProps {
+  username: string;
+  status: string;
+  profilePic: null | File;
+  aboutMe: string;
+  link: string;
+  interests: string;
+  skills: object;
+  socialMediaUrls: {
+    instagram: string;
+    facebook: string;
+    twitter: string;
+    tiktok: string;
+  };
+}
 
 const useProfile = () => {
   const mockProfile = {
     username: '',
     status: '',
-    profilePic: '',
+    profilePic: null,
     aboutMe: '',
     link: '',
     interests: '',
@@ -18,7 +34,9 @@ const useProfile = () => {
     },
   };
 
-  const [profile, setProfile] = useState(mockProfile);
+  const [profile, setProfile] = useState<profileProps>(mockProfile);
+  const [success, setSuccess] = useState(false);
+  const [hasErrors, setHasErrors] = useState(false);
 
   useEffect(() => {
     async function getProfile() {
@@ -31,6 +49,8 @@ const useProfile = () => {
 
   const handleInputChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
+      setHasErrors(false);
+      setSuccess(false);
       const { name, value } = e.target;
       setProfile({ ...profile, [name]: value });
     },
@@ -39,6 +59,8 @@ const useProfile = () => {
 
   const handleSocialMediaInputChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
+      setHasErrors(false);
+      setSuccess(false);
       const { name, value } = e.target;
       setProfile({
         ...profile,
@@ -50,16 +72,54 @@ const useProfile = () => {
 
   const handleTextAreaChange = useCallback(
     (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      setHasErrors(false);
+      setSuccess(false);
       const { name, value } = e.target;
       setProfile({ ...profile, [name]: value });
     },
     [profile],
   );
+
+  const handleProfilePicChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setHasErrors(false);
+      setSuccess(false);
+      if (e.target.files) {
+        const file = e.target.files[0];
+        setProfile({ ...profile, profilePic: file });
+      }
+    },
+    [profile],
+  );
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const formData = new FormData();
+      if (profile.profilePic !== null) {
+        formData.append('profilePic', profile.profilePic);
+      }
+
+      const response = await axios.put('/api/users/user/profile', formData);
+      if (response.status === 200) {
+        setSuccess(true);
+      }
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        setHasErrors(true);
+      }
+    }
+  };
+
   return {
     profile,
     handleInputChange,
     handleTextAreaChange,
     handleSocialMediaInputChange,
+    handleProfilePicChange,
+    success,
+    hasErrors,
+    handleSubmit,
   };
 };
 
