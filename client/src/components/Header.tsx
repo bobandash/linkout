92077903840f072d065e0f89beb5cbf0759b3522
Bruce Hsu <brucehsu1126@gmail.com>
2023-11-25 01:50:29 -0800

    Add image to header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,12 +3,23 @@ import { FC } from 'react';
 interface HeaderProps {
   name: string;
   children?: React.ReactNode;
+  image?: string;
 }
 
-const Header: FC<HeaderProps> = ({ name, children }) => {
+const Header: FC<HeaderProps> = ({ name, image, children }) => {
   return (
-    <header className="grid-in-main-header bg-color_1 lg:bg-color_3 p-7 md:p-10 md:pb-5 lg:border-b-0 2xl:p-16 2xl:pb-5">
-      <h1 className="text-outline pb-2 text-center font-play text-4xl uppercase text-white lg:text-left xl:text-6xl">
+    <header className="bg-color_1 p-7 md:p-10 md:py-5 lg:flex lg:items-center lg:border-b-0 lg:bg-color_3 2xl:p-16 2xl:py-7">
+      {image && image.length <= 2 && (
+        <div className="mr-3 hidden aspect-square h-[60px] overflow-hidden rounded-full bg-white md:my-auto lg:flex lg:items-center lg:justify-center 2xl:mr-5 2xl:h-[80px]">
+          <p className="font-bold uppercase">{image}</p>
+        </div>
+      )}
+      {image && image.length > 2 && (
+        <div className="mr-3 hidden aspect-square h-[60px] overflow-hidden rounded-full bg-white md:my-auto lg:flex lg:items-center lg:justify-center 2xl:mr-5 2xl:h-[80px]">
+          <img src={`/api/${image}`} alt={`${name} picture`} />
+        </div>
+      )}
+      <h1 className="text-outline text-center font-play text-4xl uppercase text-white lg:text-left xl:text-6xl">
         {name}
       </h1>
       {children && children}
