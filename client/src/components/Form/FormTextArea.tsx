56a39f5e84bad724e25c0d011f872d0582676ea9
Bruce Hsu <brucehsu1126@gmail.com>
2023-11-24 16:18:15 -0800

    Fix typo

--- client/src/components/Form/FormTextArea.tsx
+++ client/src/components/Form/FormTextArea.tsx
@@ -7,7 +7,7 @@ interface EditProfileInputProps {
   value: string;
   handleChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
   rows?: number;
-  required?: true;
+  required?: boolean;
 }
 
 const FormTextArea: FC<EditProfileInputProps> = ({
@@ -22,19 +22,19 @@ const FormTextArea: FC<EditProfileInputProps> = ({
     <div className="relative mt-7 w-full">
       <label
         htmlFor={nameCamelized}
-        className="text-outline bg-color_3 absolute left-0 top-0 -translate-x-1 -translate-y-1/2  pb-1 pr-2 font-play text-xl uppercase text-white"
+        className="text-outline absolute left-0 top-0 -translate-x-1 -translate-y-1/2 bg-color_3  pb-1 pr-2 font-play text-xl uppercase text-white"
       >
         {name}
-        <RequiredAsterisk />
+        {required === true && <RequiredAsterisk />}
       </label>
       <textarea
-        className="bg-color_3 w-full border-2 border-black px-2 py-3 text-xl text-white focus:outline-none"
+        className="w-full border-2 border-black bg-color_3 px-2 py-3 text-xl text-white focus:outline-none"
         name={nameCamelized}
         id={nameCamelized}
         onChange={handleChange}
         value={value}
         rows={rows && rows}
-        required={required && true}
+        required={required === true ? true : false}
       ></textarea>
     </div>
   );
