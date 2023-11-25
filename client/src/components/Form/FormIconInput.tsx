7d15dd9e12d6280bdbbb7894b859ab1686fa078a
Bruce Hsu <brucehsu1126@gmail.com>
2023-11-24 16:18:27 -0800

    Make image show when click upload file

--- client/src/components/Form/FormIconInput.tsx
+++ client/src/components/Form/FormIconInput.tsx
@@ -1,20 +1,50 @@
-import { FC } from 'react';
+import { FC, useRef } from 'react';
+import camelize from '../../utils/camelize';
 
 interface FormIconInputProps {
   centered?: boolean;
+  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  name: string;
 }
 
-const FormIconInput: FC<FormIconInputProps> = ({ centered }) => {
+const FormIconInput: FC<FormIconInputProps> = ({
+  centered,
+  handleInputChange,
+  name,
+}) => {
+  const nameCamelized = camelize(name);
+  const imageRef = useRef<HTMLImageElement | null>(null);
+  const loadFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (imageRef.current !== null && e.target.files !== null) {
+      imageRef.current.src = URL.createObjectURL(e.target.files[0]);
+      imageRef.current.onload = function () {
+        if (imageRef.current !== null) {
+          URL.revokeObjectURL(imageRef.current.src);
+        }
+      };
+    }
+  };
+
   return (
     <div
-      className={`relative aspect-square max-w-[80px] overflow-hidden rounded-full border-2 bg-white md:max-w-[100px] ${
+      className={`relative flex aspect-square max-w-[80px] overflow-hidden rounded-full border-2 bg-white hover:cursor-pointer md:max-w-[100px] ${
         centered && 'mx-auto'
       }`}
     >
-      <img alt="picture" />
+      <img
+        alt="picture"
+        className="flex-grow hover:cursor-pointer "
+        ref={imageRef}
+      />
       <input
+        onChange={(e) => {
+          loadFile(e);
+          handleInputChange(e);
+        }}
         type="file"
         className="absolute left-0 top-0 h-full w-full opacity-0 hover:cursor-pointer"
+        name={nameCamelized}
+        id={nameCamelized}
         accept="image/png, image/jpeg"
       />
     </div>
