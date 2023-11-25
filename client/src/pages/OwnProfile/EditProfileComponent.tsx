d9321d5d0975ce1be35c0a5b28090c984f196b3d
Bruce Hsu <brucehsu1126@gmail.com>
2023-11-24 16:17:55 -0800

    Use generic form inputs instead of specific form

--- client/src/pages/OwnProfile/EditProfileComponent.tsx
+++ client/src/pages/OwnProfile/EditProfileComponent.tsx
@@ -1,8 +1,7 @@
 import useProfile from './__hooks__/useProfile';
-import EditProfileInput from './components/EditProfileInput';
-import EditProfileTextArea from './components/EditProfileTextArea';
-import ProfilePicForm from './components/ProfilePicForm';
-// TO-DO: add skills functionality
+import FormInput from '../../components/Form/FormInput';
+import FormTextArea from '../../components/Form/FormTextArea';
+import FormIconInput from '../../components/Form/FormIconInput';
 
 const EditProfileComponent = () => {
   const {
@@ -10,74 +9,84 @@ const EditProfileComponent = () => {
     handleInputChange,
     handleTextAreaChange,
     handleSocialMediaInputChange,
+    handleProfilePicChange,
+    handleSubmit,
+    success,
   } = useProfile();
-  const {
-    username,
-    status,
-    profilePic,
-    aboutMe,
-    link,
-    interests,
-    socialMediaUrls,
-  } = profile;
+  const { username, status, aboutMe, link, interests, socialMediaUrls } =
+    profile;
 
   return (
     <div className="flex flex-col items-center p-5">
-      <ProfilePicForm profilePic={profilePic} />
-      <form className="flex w-full flex-col items-center gap-3" noValidate>
-        <EditProfileInput
+      <form
+        className="flex w-full flex-col items-center gap-3"
+        noValidate
+        autoComplete="off"
+        encType="multipart/form-data"
+        onSubmit={async (e) => {
+          await handleSubmit(e);
+        }}
+      >
+        {/* TO-DO: add preexisting profile pic */}
+        <FormIconInput
+          name={'Profile Pic'}
+          handleInputChange={handleProfilePicChange}
+        />
+        <p>Choose Profile pic</p>
+        <FormInput
           name={'Username'}
           autoFocus={true}
           value={username}
           handleInputChange={handleInputChange}
+          required={true}
         />
-        <EditProfileInput
+        <FormInput
           name={'Status'}
           value={status}
           handleInputChange={handleInputChange}
         />
-        <EditProfileInput
+        <FormInput
           name={'About Me'}
           value={aboutMe}
           handleInputChange={handleInputChange}
         />
-        <EditProfileInput
+        <FormInput
           name={'Link'}
           value={link}
           handleInputChange={handleInputChange}
         />
-        <EditProfileTextArea
+        <FormTextArea
           name={'Interests'}
           value={interests}
           handleChange={handleTextAreaChange}
         />
         <div className="relative w-full border-4 border-black">
           <h1
-            className="text-outline bg-color_3 absolute left-0 top-0 -translate-x-1
-          -translate-y-1/2 pb-1 pr-2 font-play text-xl uppercase text-white"
+            className="text-outline absolute left-0 top-0 -translate-x-1 -translate-y-1/2
+          bg-color_3 pb-1 pr-2 font-play text-xl uppercase text-white"
           >
             Social Media Icons
           </h1>
           <div className="p-5">
-            <EditProfileInput
+            <FormInput
               name={'Instagram'}
               value={socialMediaUrls.instagram}
               handleInputChange={handleSocialMediaInputChange}
               placeholder={'instagram.com/yourusername/'}
             />
-            <EditProfileInput
+            <FormInput
               name={'Facebook'}
               value={socialMediaUrls.facebook}
               handleInputChange={handleSocialMediaInputChange}
               placeholder={'facebook.com/yourusername/'}
             />
-            <EditProfileInput
+            <FormInput
               name={'Twitter'}
               value={socialMediaUrls.twitter}
               handleInputChange={handleSocialMediaInputChange}
               placeholder={'twitter.com/yourusername/'}
             />
-            <EditProfileInput
+            <FormInput
               name={'Tiktok'}
               value={socialMediaUrls.tiktok}
               handleInputChange={handleSocialMediaInputChange}
@@ -87,10 +96,11 @@ const EditProfileComponent = () => {
         </div>
         <button
           type="submit"
-          className="w-full rounded-xl border-2 bg-secondary px-4 py-2 font-play text-xl font-bold uppercase md:ml-auto md:w-fit"
+          className="w-full rounded-xl border-2 border-black bg-primary px-4 py-2 font-play text-xl font-bold uppercase text-white md:ml-auto md:w-fit"
         >
           Save
         </button>
+        {success && <p>Profile has successfully been updated.</p>}
       </form>
     </div>
   );
