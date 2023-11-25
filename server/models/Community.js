1543ba58ab4e4661f1ac3d7732e7cd6f10f4c1bb
Bruce Hsu <brucehsu1126@gmail.com>
2023-11-24 16:18:44 -0800

    Add num users to virtual method

--- server/models/Community.js
+++ server/models/Community.js
@@ -2,43 +2,53 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const CommunityMessageSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  profilePic: {
-    type: String,
-    required: true,
-  },
-  Users: [
-    {
-      user: {
+const CommunitySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    profilePic: {
+      type: String,
+      required: true,
+    },
+    Users: [
+      {
+        user: {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+        isVisible: {
+          type: Boolean,
+          default: true,
+        },
+      },
+    ],
+    Admins: [
+      {
         type: Schema.Types.ObjectId,
         ref: 'User',
       },
-      isVisible: {
-        type: Boolean,
-        default: true,
+    ],
+    LinkOuts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Linkout',
       },
-    },
-  ],
-  Admins: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-    },
-  ],
-  LinkOuts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Linkout',
-    },
-  ],
+    ],
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+CommunitySchema.virtual('numUsers').get(function () {
+  return this.Users.length;
 });
 
-module.exports = mongoose.model('Community', CommunityMessageSchema);
+module.exports = mongoose.model('Community', CommunitySchema);
