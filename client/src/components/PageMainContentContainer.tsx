d492b0fdb9ebc9df5b214d66cd34fd913997e8be
Bruce Hsu <brucehsu1126@gmail.com>
2023-11-27 12:52:18 -0800

    Add minor css fixes

--- client/src/components/PageMainContentContainer.tsx
+++ client/src/components/PageMainContentContainer.tsx
@@ -6,7 +6,7 @@ interface PageTemplateProps {
 
 const PageMainContentContainer: FC<PageTemplateProps> = ({ children }) => {
   return (
-    <div className="bg-color_3 flex flex-grow flex-col p-5 md:p-7 lg:p-10 lg:pt-0 2xl:p-16 2xl:pt-0">
+    <div className="flex flex-grow flex-col bg-color_3 p-5 md:p-7 lg:p-10 lg:pt-0 2xl:p-16 2xl:pt-0">
       {children}
     </div>
   );
