17d6a83ca75dfe947b6c0f111d1ad0b4464d129f
Bruce Hsu <brucehsu1126@gmail.com>
2023-12-06 23:14:50 -0800

    Add logout button

---
+++ client/src/pages/OwnProfile/components/ProfileButtons/LogoutButton.tsx
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const LogoutButton = () => {
+  async function logout() {
+    await axios.post('/api/logout', {
+      withCredentials: true,
+    });
+    window.location.href = '/';
+  }
+
+  return (
+    <button
+      onClick={async () => {
+        await logout();
+      }}
+      className="text-outline mt-1 block rounded-lg border-2 border-white bg-red-500 px-3 py-2 font-play text-xl uppercase text-white lg:ml-auto lg:mr-0"
+    >
+      Logout
+    </button>
+  );
+};
+
+export default LogoutButton;
