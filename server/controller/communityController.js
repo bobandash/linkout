0b83d53a8e33c7a22db3180b70e878371541ce81
Bruce Hsu <brucehsu1126@gmail.com>
2023-12-06 23:20:17 -0800

    Add try catch to await methods

--- server/controller/communityController.js
+++ server/controller/communityController.js
@@ -8,6 +8,7 @@ const verifyToken = require('./utils/verifyToken');
 const multer = require('multer');
 const path = require('path');
 const CommunityMessage = require('../models/CommunityMessage');
+const verifyInCommunity = require('./utils/verifyInCommunity');
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'public/uploads/');
@@ -21,39 +22,46 @@ const upload = multer({ storage: storage });
 exports.get_communities = [
   verifyToken,
   async (req, res, next) => {
-    const { limit, numUsersOrder, nameOrder, communityId } = req.query;
+    try {
+      const { limit, numUsersOrder, nameOrder, communityId } = req.query;
 
-    let communities = Community.find({});
+      let communities = Community.find({});
 
-    if (numUsersOrder) {
-      communities = communities.sort({ numUsers: Number(numUsersOrder) });
-    } else if (nameOrder) {
-      communities = communities.sort({ name: Number(nameOrder) });
+      if (numUsersOrder) {
+        communities = communities.sort({ numUsers: Number(numUsersOrder) });
+      } else if (nameOrder) {
+        communities = communities.sort({ name: Number(nameOrder) });
+      }
+
+      communities = communities.select({
+        name: 1,
+        description: 1,
+        profilePic: 1,
+        users: 1,
+      });
+
+      if (limit) {
+        communities = communities.limit(Number(limit));
+      }
+      communities = await communities.exec();
+      return res.json({ communities: communities });
+    } catch {
+      return res.status(404).json({ error: 'Cannot find' });
     }
-
-    communities = communities.select({
-      name: 1,
-      description: 1,
-      profilePic: 1,
-      users: 1,
-    });
-
-    if (limit) {
-      communities = communities.limit(Number(limit));
-    }
-
-    communities = await communities.exec();
-
-    res.json({ communities: communities });
   },
 ];
 
 exports.get_individual_community = [
   verifyToken,
+  verifyInCommunity,
   async (req, res, next) => {
-    const { communityId } = req.params;
-    const community = await Community.findById(communityId).exec();
-    res.json(community);
+    try {
+      const { communityId } = req.params;
+      const community = await Community.findById(communityId).exec();
+      return res.json(community);
+    } catch {
+      return res.status(404).json({ error: 'Cannot find' });
+    }
   },
 ];
 
@@ -66,32 +74,38 @@ exports.create_community = [
     .notEmpty()
     .escape(),
   async (req, res, next) => {
-    const errors = validationResult(req).mapped();
-    const { name, description } = req.body;
-    if (Object.keys(errors).length > 0) {
-      return res.status(400).json(errors);
+    try {
+      const errors = validationResult(req).mapped();
+      const { name, description } = req.body;
+      if (Object.keys(errors).length > 0) {
+        return res.status(400).json(errors);
+      }
+      const { email } = req.user;
+      const user = await User.findOne({ email: email }).exec();
+      let community = {};
+      const defaultPfp = name.length === 1 ? name : name.substring(0, 2);
+      let image = req.file
+        ? path.join('uploads', req.file.filename)
+        : defaultPfp;
+      community = new Community({
+        name,
+        description,
+        profilePic: image,
+        users: [
+          {
+            user: user,
+            isVisible: true,
+          },
+        ],
+        admins: [user],
+        linkOuts: [],
+      });
+      await user.updateOne({ $push: { communities: community } }).exec();
+      await community.save();
+      return res.json({ community: community });
+    } catch {
+      return res.status(404).json({ error: 'Cannot find' });
     }
-    const { email } = req.user;
-    const user = await User.findOne({ email: email }).exec();
-    let community = {};
-    const defaultPfp = name.length === 1 ? name : name.substring(0, 2);
-    let image = req.file ? path.join('uploads', req.file.filename) : defaultPfp;
-    community = new Community({
-      name,
-      description,
-      profilePic: image,
-      users: [
-        {
-          user: user,
-          isVisible: true,
-        },
-      ],
-      admins: [user],
-      linkOuts: [],
-    });
-    await user.updateOne({ $push: { communities: community } }).exec();
-    await community.save();
-    return res.json({ community: community });
   },
 ];
 
@@ -99,56 +113,65 @@ exports.add_message = [
   verifyToken,
   body('message', 'Message must not be empty').trim().notEmpty().escape(),
   async (req, res, next) => {
-    const errors = validationResult(req).mapped();
-    if (Object.keys(errors).length > 0) {
-      return res.status(400).json(errors);
+    try {
+      const errors = validationResult(req).mapped();
+      if (Object.keys(errors).length > 0) {
+        return res.status(400).json(errors);
+      }
+      const { email } = req.user;
+      const { communityId } = req.params;
+      const { message } = req.body;
+      const [community, user] = await Promise.all([
+        Community.findById(communityId).exec(),
+        User.findOne({ email: email }).exec(),
+      ]);
+      const newMessage = new CommunityMessage({
+        content: message,
+        sender: user,
+        community: community,
+      });
+      await newMessage.save();
+      // Need to format the message with relevant fields
+      const messageToReturn = await CommunityMessage.findById(newMessage._id)
+        .populate({
+          path: 'sender',
+          select: 'profile',
+          populate: {
+            path: 'profile',
+            select: 'profilePic username',
+          },
+        })
+        .exec();
+      res.json({ message: messageToReturn });
+    } catch {
+      return res.status(404).json({ error: 'Cannot find' });
     }
-    const { email } = req.user;
-    const { communityId } = req.params;
-    const { message } = req.body;
-    const [community, user] = await Promise.all([
-      Community.findById(communityId).exec(),
-      User.findOne({ email: email }).exec(),
-    ]);
-    const newMessage = new CommunityMessage({
-      content: message,
-      sender: user,
-      community: community,
-    });
-    await newMessage.save();
-    // Need to format the message with relevant fields
-    const messageToReturn = await CommunityMessage.findById(newMessage._id)
-      .populate({
-        path: 'sender',
-        select: 'profile',
-        populate: {
-          path: 'profile',
-          select: 'profilePic username',
-        },
-      })
-      .exec();
-    res.json({ message: messageToReturn });
   },
 ];
 
 exports.get_messages = [
   verifyToken,
+  verifyInCommunity,
   async (req, res, next) => {
-    const { communityId } = req.params;
-    const messages = await CommunityMessage.find({
-      community: communityId,
-    })
-      .populate({
-        path: 'sender',
-        select: 'profile',
-        populate: {
-          path: 'profile',
-          select: 'profilePic username',
-        },
+    try {
+      const { communityId } = req.params;
+      const messages = await CommunityMessage.find({
+        community: communityId,
       })
-      .sort({ createdAt: 1 })
-      .exec();
-    res.json(messages);
+        .populate({
+          path: 'sender',
+          select: 'profile',
+          populate: {
+            path: 'profile',
+            select: 'profilePic username',
+          },
+        })
+        .sort({ createdAt: 1 })
+        .exec();
+      res.json(messages);
+    } catch {
+      return res.status(404).json({ error: 'Cannot find' });
+    }
   },
 ];
 
@@ -156,31 +179,35 @@ exports.add_image = [
   verifyToken,
   upload.single('image'),
   async (req, res, next) => {
-    let imagePath = path.join('uploads', req.file.filename);
-    const { email } = req.user;
-    const { communityId } = req.params;
-    const [community, user] = await Promise.all([
-      Community.findById(communityId).exec(),
-      User.findOne({ email: email }).exec(),
-    ]);
+    try {
+      let imagePath = path.join('uploads', req.file.filename);
+      const { email } = req.user;
+      const { communityId } = req.params;
+      const [community, user] = await Promise.all([
+        Community.findById(communityId).exec(),
+        User.findOne({ email: email }).exec(),
+      ]);
 
-    const newMessage = new CommunityMessage({
-      image: imagePath,
-      sender: user,
-      community: community,
-    });
-    await newMessage.save();
-    // Need to format the message with relevant fields
-    const messageToReturn = await CommunityMessage.findById(newMessage._id)
-      .populate({
-        path: 'sender',
-        select: 'profile',
-        populate: {
-          path: 'profile',
-          select: 'profilePic username',
-        },
-      })
-      .exec();
-    res.json({ message: messageToReturn });
+      const newMessage = new CommunityMessage({
+        image: imagePath,
+        sender: user,
+        community: community,
+      });
+      await newMessage.save();
+      // Need to format the message with relevant fields
+      const messageToReturn = await CommunityMessage.findById(newMessage._id)
+        .populate({
+          path: 'sender',
+          select: 'profile',
+          populate: {
+            path: 'profile',
+            select: 'profilePic username',
+          },
+        })
+        .exec();
+      res.json({ message: messageToReturn });
+    } catch {
+      return res.status(404).json({ error: 'Cannot find' });
+    }
   },
 ];
