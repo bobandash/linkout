bac174fcccf63416d91d2761ab9b553f7999109f
Bruce Hsu <brucehsu1126@gmail.com>
2023-12-06 23:30:52 -0800

    Change css

--- client/src/pages/OwnProfile/components/ProfileButtons/PreviewButton.tsx
+++ client/src/pages/OwnProfile/components/ProfileButtons/PreviewButton.tsx
@@ -9,7 +9,7 @@ const PreviewButton = () => {
   return (
     <button
       onClick={navigateView}
-      className="'text-outline mx-auto mt-1 block rounded-lg border-2 border-white bg-black px-3 py-2 font-play text-xl uppercase text-white lg:ml-auto lg:mr-0"
+      className="'text-outline mt-1 block rounded-lg border-2 border-white bg-black px-3 py-2 font-play text-xl uppercase text-white lg:ml-auto lg:mr-0"
     >
       Preview
     </button>
