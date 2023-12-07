e0e91ae5a3cc940f7bf956a4f11b974b6b5cee28
Bruce Hsu <brucehsu1126@gmail.com>
2023-12-06 23:31:16 -0800

    Add logout button to own profile

--- client/src/pages/OwnProfile/EditProfile.tsx
+++ client/src/pages/OwnProfile/EditProfile.tsx
@@ -3,11 +3,20 @@ import Header from '../../components/Header';
 import PageMainContentContainer from '../../components/PageMainContentContainer';
 import EditProfileComponent from './components/EditProfileComponent';
 import PreviewButton from './components/ProfileButtons/PreviewButton';
+import LogoutButton from './components/ProfileButtons/LogoutButton';
 
 const Profile = () => {
   return (
     <div className="flex min-h-screen flex-col md:h-screen">
-      <Header name={'Your Profile'} children={<PreviewButton />} />
+      <Header
+        name={'Your Profile'}
+        children={
+          <div className="ml-auto mt-2 flex flex-row justify-center gap-2 md:gap-5">
+            <PreviewButton />
+            <LogoutButton />
+          </div>
+        }
+      />
       <PageMainContentContainer>
         <EditProfileComponent />
       </PageMainContentContainer>
