d51cbc99a1e38f3d84222f03c1925f6a3cca33d5
Bruce Hsu <brucehsu1126@gmail.com>
2023-12-14 21:33:17 -0800

    Add overflow hidden to container

--- client/src/components/PageTemplate.tsx
+++ client/src/components/PageTemplate.tsx
@@ -8,7 +8,7 @@ interface PageTemplateProps {
 
 const PageTemplate: FC<PageTemplateProps> = ({ children }) => {
   return (
-    <div className="grid-cols-default_site lg:grid-cols-lg_site xl:grid-cols-xl_site 2xl:grid-cols-two_xl_site grid-rows-mobile_site grid">
+    <div className="grid-cols-default_site xl:grid-cols-xl_site grid-rows-mobile_site grid overflow-hidden lg:grid-cols-lg_site 2xl:grid-cols-two_xl_site">
       <Sidebar />
       {children}
       <OnlineSidebar />
