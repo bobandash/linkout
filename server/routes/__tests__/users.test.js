95f0497d9b937a1f5ea51d3931cc7fef33e91391
Bruce Hsu <brucehsu1126@gmail.com>
2024-01-31 14:06:08 -0800

    Add integration test for creating users

--- server/routes/__tests__/users.test.js
+++ server/routes/__tests__/users.test.js
@@ -18,83 +18,82 @@ afterEach(async () => {
   await deleteMongoServer();
 });
 
-describe('POST /users/create', () => {
-  it('works with valid form data', async () => {
-    const response = await request(app)
-      .post('/users/create')
-      .type('form')
-      .send({
+describe('Test user API', () => {
+  describe('POST /users/create', () => {
+    it('should create user with valid data', async () => {
+      const response = await request(app)
+        .post('/users/create')
+        .type('form')
+        .send({
+          email: 'testing@gmail.com',
+          password: 'ipisJn8I4A2Mx7Q!',
+          confirmPassword: 'ipisJn8I4A2Mx7Q!',
+        });
+      expect(response.status).toEqual(200);
+    });
+
+    it(`should send error message when email already exists`, async () => {
+      await request(app).post('/users/create').type('form').send({
         email: 'testing@gmail.com',
         password: 'ipisJn8I4A2Mx7Q!',
+        displayName: 'Test',
         confirmPassword: 'ipisJn8I4A2Mx7Q!',
       });
-    expect(response.status).toEqual(200);
-  });
-
-  it(`doesn't work when user already exists`, async () => {
-    await request(app).post('/users/create').type('form').send({
-      email: 'testing@gmail.com',
-      password: 'ipisJn8I4A2Mx7Q!',
-      displayName: 'Test',
-      confirmPassword: 'ipisJn8I4A2Mx7Q!',
+      const response = await request(app)
+        .post('/users/create')
+        .type('form')
+        .send({
+          email: 'testing@gmail.com',
+          password: 'ipisJn8I4A2Mx7Q!',
+          confirmPassword: 'ipisJn8I4A2Mx7Q!',
+        });
+      expect(response.status).toEqual(400);
     });
-    const response = await request(app)
-      .post('/users/create')
-      .type('form')
-      .send({
-        email: 'testing@gmail.com',
-        password: 'ipisJn8I4A2Mx7Q!',
-        confirmPassword: 'ipisJn8I4A2Mx7Q!',
-      });
-    expect(response.status).toEqual(400);
-    expect(response.body).toEqual({
-      email: { msg: 'Email already has an account' },
-    });
-  });
 
-  it(`doesn't work when confirm password doesn't match`, async () => {
-    const response = await request(app)
-      .post('/users/create')
-      .type('form')
-      .send({
-        email: 'testing@gmail.com',
-        password: 'ipisJn8I4A2Mx7Q!',
-        confirmPassword: 'test',
+    it(`should send error message when confirm password doesn't match password`, async () => {
+      const response = await request(app)
+        .post('/users/create')
+        .type('form')
+        .send({
+          email: 'testing@gmail.com',
+          password: 'ipisJn8I4A2Mx7Q!',
+          confirmPassword: 'test',
+        });
+      expect(response.status).toEqual(400);
+      expect(response.body).toEqual({
+        confirmPassword: {
+          msg: 'Passwords do not match',
+        },
       });
-    expect(response.status).toEqual(400);
-    expect(response.body).toEqual({
-      confirmPassword: {
-        msg: 'Passwords do not match',
-      },
     });
-  });
 
-  it(`doesn't work when password & email aren't valid`, async () => {
-    const response = await request(app)
-      .post('/users/create')
-      .type('form')
-      .send({
-        email: 'testing',
-        password: '123!',
-        confirmPassword: '123!',
+    it(`should send error message when password and username don't match`, async () => {
+      const response = await request(app)
+        .post('/users/create')
+        .type('form')
+        .send({
+          email: 'testing',
+          password: '123!',
+          confirmPassword: '123!',
+        });
+      expect(response.status).toEqual(400);
+      expect(response.body).toEqual({
+        email: {
+          msg: 'Email already has an account',
+          location: 'body',
+          msg: 'Email is invalid',
+          path: 'email',
+          type: 'field',
+          value: 'testing',
+        },
+        password: {
+          location: 'body',
+          msg: 'Password has to be at least 8 characters, 1 uppercase, and 1 symbol',
+          path: 'password',
+          type: 'field',
+          value: '123!',
+        },
       });
-    expect(response.status).toEqual(400);
-    expect(response.body).toEqual({
-      email: {
-        msg: 'Email already has an account',
-        location: 'body',
-        msg: 'Email is invalid',
-        path: 'email',
-        type: 'field',
-        value: 'testing',
-      },
-      password: {
-        location: 'body',
-        msg: 'Password has to be at least 8 characters, 1 uppercase, and 1 symbol',
-        path: 'password',
-        type: 'field',
-        value: '123!',
-      },
     });
   });
 });
