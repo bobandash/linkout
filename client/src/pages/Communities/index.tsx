01c3a942a0a01884b41e2df7dd2f5727e44230e0
Bruce Hsu <brucehsu1126@gmail.com>
2024-02-10 16:51:47 -0800

    Change create community button location

--- client/src/pages/Communities/index.tsx
+++ client/src/pages/Communities/index.tsx
@@ -1,6 +1,6 @@
 import Header from '../../components/Header';
 import CommunitiesComponent from './CommunitiesComponent';
-import CommunitiesCreateBtn from '../../components/HeaderMobileRedirectBtns/CommunitiesCreateBtn';
+import { useNavigate } from 'react-router';
 import Footer from '../../components/Footer';
 import useCommunities from './__hooks__/useCommunities';
 import LoadingScreen from '../Loading/index';
@@ -21,6 +21,24 @@ const CommunitiesPage = () => {
       />
       <Footer />
     </div>
+  );
+};
+
+const CommunitiesCreateBtn = () => {
+  const navigate = useNavigate();
+  function navCreateCommunities() {
+    navigate('/communities/create');
+  }
+
+  return (
+    <button
+      onClick={navCreateCommunities}
+      className={
+        'text-outline mx-auto mt-1 block rounded-lg border-2 border-white bg-black px-3 py-2 font-play text-xl uppercase text-white lg:ml-auto lg:mr-0'
+      }
+    >
+      Create Your Own
+    </button>
   );
 };
 
