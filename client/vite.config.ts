482608bdbdef023393d891f0c54d534e4c7fca77
Bruce Hsu <brucehsu1126@gmail.com>
2024-02-11 16:04:12 -0800

    Add coverage and update vitest

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
+    coverage: {
+      provider: 'v8',
+    },
     globals: true,
     environment: 'jsdom',
     setupFiles: './tests/setup.ts',
