be45e42a7747c968dd1ed089337ec4345157caca
Bruce Hsu <brucehsu1126@gmail.com>
2024-02-11 19:37:52 -0800

    Create error version

--- client/src/components/LeftSidebar/components/Username.tsx
+++ client/src/components/LeftSidebar/components/Username.tsx
@@ -7,6 +7,7 @@ import ProfilePic from '../../ProfilePic';
 const Username = () => {
   const navigate = useNavigate();
   const [profile, setProfile] = useState<profileProps>(mockProfile);
+  const [hasError, setHasError] = useState(false);
   function handleEditProfile() {
     navigate('/profile/edit');
   }
@@ -20,20 +21,38 @@ const Username = () => {
         const profile = response.data.profile;
         setProfile(profile);
       } catch {
-        console.error('There was an error');
+        console.error('There was an error fetching the profile');
+        setHasError(true);
       }
     }
     getProfile();
   }, []);
 
+  if (hasError) {
+    return (
+      <div className="bg-secondary flex w-full flex-row gap-x-4 border-b-4 border-black p-5 grid-in-sidebar-header">
+        <div className="flex items-center justify-center">
+          <div className="aspect-square min-w-[60px] rounded-full bg-white 2xl:min-w-[80px]"></div>
+        </div>
+        <div className="flex flex-col justify-center overflow-hidden">
+          <h1 className="text-outline xl:text-1xl overflow-ellipsis font-play font-bold uppercase text-white md:max-w-[10ch] md:overflow-hidden md:overflow-ellipsis md:text-xl">
+            ERROR LOADING
+          </h1>
+          <button
+            onClick={handleEditProfile}
+            className="w-max font-play uppercase text-white"
+          >
+            Edit Profile
+          </button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="bg-secondary flex w-full flex-row gap-x-4 border-b-4 border-black p-5 grid-in-sidebar-header">
       <div className="flex items-center justify-center">
-        {typeof profile.profilePic === 'string' ? (
-          <ProfilePic name={profile.username} image={profile.profilePic} />
-        ) : (
-          <div className="aspect-square min-w-[60px] rounded-full bg-white 2xl:min-w-[80px]"></div>
-        )}
+        <ProfilePic name={profile.username} image={profile.profilePic} />
       </div>
       <div className="flex flex-col justify-center overflow-hidden">
         <h1 className="text-outline xl:text-1xl overflow-ellipsis font-play font-bold uppercase text-white md:max-w-[10ch] md:overflow-hidden md:overflow-ellipsis md:text-xl">
