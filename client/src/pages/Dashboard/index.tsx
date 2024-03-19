737946b1d9aa8c24514f38411458ce2aae29ebd5
Bruce <bruce@personaify.ai>
2024-03-19 16:10:27 -0700

    Fix navigate warning

--- client/src/pages/Dashboard/index.tsx
+++ client/src/pages/Dashboard/index.tsx
@@ -1,14 +1,17 @@
 import { Outlet, useNavigate } from 'react-router';
 import PageTemplate from '../../components/PageTemplate';
 import { useAuthContext } from '../../context/useAuthContext';
+import { useEffect } from 'react';
 
 const Dashboard = () => {
-  const { isSignedIn } = useAuthContext();
+  const { isSignedIn, isAuthLoading } = useAuthContext();
   const navigate = useNavigate();
 
-  if (!isSignedIn) {
-    navigate('/');
-  }
+  useEffect(() => {
+    if (!isSignedIn && !isAuthLoading) {
+      navigate('/');
+    }
+  }, [isSignedIn, navigate, isAuthLoading]);
 
   return (
     <PageTemplate>
