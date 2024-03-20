82b532bdbb238bc40cfaa84bc386d89c980f6a36
Bruce <bruce@personaify.ai>
2024-03-19 23:48:33 -0700

    Uncomment out AuthLoading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,18 @@
 import SignInForm from './pages/SignInForm/index';
 import LoadingScreen from './pages/Loading/index';
-// import { useNavigate } from 'react-router';
-// import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import { useEffect } from 'react';
 import { useAuthContext } from './context/useAuthContext';
 
 function App() {
-  // const { isSignedIn, isAuthLoading } = useAuthContext();
-  // const navigate = useNavigate();
+  const { isSignedIn, isAuthLoading } = useAuthContext();
+  const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   if (!isAuthLoading && isSignedIn) {
-  //     navigate('/dashboard');
-  //   }
-  // }, [isAuthLoading, isSignedIn, navigate]);
-
-  const { isAuthLoading } = useAuthContext();
+  useEffect(() => {
+    if (!isAuthLoading && isSignedIn) {
+      navigate('/dashboard');
+    }
+  }, [isAuthLoading, isSignedIn, navigate]);
 
   if (isAuthLoading) {
     return <LoadingScreen />;
