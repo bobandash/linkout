bdd05d303b301d7557efa2e5d9b58825f5bce17b
Bruce <bruce@personaify.ai>
2024-03-19 22:58:13 -0700

    Comment out redirect to figure out issue with cookie

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,20 @@
 import SignInForm from './pages/SignInForm/index';
 import LoadingScreen from './pages/Loading/index';
-import { useNavigate } from 'react-router';
+// import { useNavigate } from 'react-router';
+// import { useEffect } from 'react';
 import { useAuthContext } from './context/useAuthContext';
-import { useEffect } from 'react';
 
 function App() {
-  const { isSignedIn, isAuthLoading } = useAuthContext();
-  const navigate = useNavigate();
+  // const { isSignedIn, isAuthLoading } = useAuthContext();
+  // const navigate = useNavigate();
 
-  useEffect(() => {
-    if (!isAuthLoading && isSignedIn) {
-      navigate('/dashboard');
-    }
-  }, [isAuthLoading, isSignedIn, navigate]);
+  // useEffect(() => {
+  //   if (!isAuthLoading && isSignedIn) {
+  //     navigate('/dashboard');
+  //   }
+  // }, [isAuthLoading, isSignedIn, navigate]);
+
+  const { isAuthLoading } = useAuthContext();
 
   if (isAuthLoading) {
     return <LoadingScreen />;
